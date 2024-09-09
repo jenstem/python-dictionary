301b087c079a8eee3fd66d0e9e0e1dd965712a09
jenstem <jennifer.stemkowski@gmail.com>
2024-09-09 11:14:03 -0400

    Define entry widget for word input by user

--- dictionary.py
+++ dictionary.py
@@ -10,8 +10,17 @@ root.geometry("1250x750")
 
 def dict():
     '''
-    This function is used to get the meaning, synonym and antonym of the word
+    This function is used to set the text for the meaning, synonym and antonym of the word
     '''
     meaning.config(text=dictionary.meaning('en', word.get())[1])
     synonym.config(text=dictionary.synonym('en', word.get()))
     antonym.config(text=dictionary.antonym('en', word.get()))
+
+
+Label(root, text="Dictionary", font=("Arial 20 bold"), fg="Green").pack(pady=10)
+
+frame = Frame(root)
+Label(frame, text="Enter Word", font=("Arial 15 bold")).pack(side=LEFT)
+word = Entry(frame, font=("Arial 15 bold"))
+word.pack()
+frame.pack(pady=10)
