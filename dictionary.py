556b57e8dea3778af7badcc3805d634479f76118
jenstem <jennifer.stemkowski@gmail.com>
2024-09-09 11:08:08 -0400

    Define func dict() to set text for meaning, synonym + antonym

--- dictionary.py
+++ dictionary.py
@@ -6,3 +6,12 @@ dictionary = MultiDictionary()
 root = Tk()
 root.title("Dictionary")
 root.geometry("1250x750")
+
+
+def dict():
+    '''
+    This function is used to get the meaning, synonym and antonym of the word
+    '''
+    meaning.config(text=dictionary.meaning('en', word.get())[1])
+    synonym.config(text=dictionary.synonym('en', word.get()))
+    antonym.config(text=dictionary.antonym('en', word.get()))
