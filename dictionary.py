4c12196aa727681cbf48d3dc15d44a68dd6b569d
jenstem <jennifer.stemkowski@gmail.com>
2024-09-09 11:28:03 -0400

    Define display widget for word meaning

--- dictionary.py
+++ dictionary.py
@@ -20,7 +20,19 @@ def dict():
 Label(root, text="Dictionary", font=("Arial 20 bold"), fg="Green").pack(pady=10)
 
 frame = Frame(root)
+'''
+This frame is used to take the input from the user
+'''
 Label(frame, text="Enter Word", font=("Arial 15 bold")).pack(side=LEFT)
 word = Entry(frame, font=("Arial 15 bold"))
 word.pack()
 frame.pack(pady=10)
+
+frame1 = Frame(root)
+'''
+This frame is used to display the meaning of the word
+'''
+Label(frame1, text="Meaning: ", font=("Arial 14 bold")).pack(side=LEFT)
+meaning = Label(frame1, text="", font=("Arial 14"),wraplength=1000)
+meaning.pack()
+frame1.pack(pady=15)
